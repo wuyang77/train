60641831d33022dd95cd610083e3d82456c5daf9
wuyang77 <2388958622@qq.com>
2026-01-08 14:51:23 +0800

    新增加member模块

---
+++ member/src/main/java/org/wuyang/member/MemberApplication.java
@@ -0,0 +1,24 @@
+package org.wuyang.member;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("org.wuyang")
+public class MemberApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(MemberApplication.class);
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        logger.info("启动成功！！");
+        logger.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+
+    }
+
+}
