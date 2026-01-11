d4b78161c44dfe25d558a8f487a3242ff10c43ee
wuyang77 <2388958622@qq.com>
2026-01-11 15:54:56 +0800

    实现1个简单的注册接口

--- member/src/main/java/org/wuyang/member/controller/TestController.java
+++ member/src/main/java/org/wuyang/member/controller/TestController.java
@@ -2,6 +2,7 @@ package org.wuyang.member.controller;
 
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wuyang.member.service.MemberService;
@@ -15,5 +16,10 @@ public class TestController {
     @GetMapping("/count")
     public Integer count () {
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public Long register (String mobile) {
+        return memberService.register(mobile);
     }
 }
--- member/src/main/java/org/wuyang/member/service/MemberService.java
+++ member/src/main/java/org/wuyang/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package org.wuyang.member.service;
 
+import cn.hutool.core.collection.CollUtil;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import org.wuyang.member.domain.Member;
+import org.wuyang.member.domain.MemberExample;
 import org.wuyang.member.mapper.MemberMapper;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,5 +17,22 @@ public class MemberService {
 
     public Integer count() {
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public Long register(String mobile) {
+
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> memberList = memberMapper.selectByExample(memberExample);
+        if (CollUtil.isNotEmpty(memberList)) {
+//            return memberList.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
