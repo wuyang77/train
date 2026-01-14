1ed4d57de76d636b7e91962970938456950bd6e4
wuyang77 <2388958622@qq.com>
2026-01-14 20:53:57 +0800

    为项目增加统一的异常处理

---
+++ common/src/main/java/org/wuyang/common/Exception/ControllerExceptionHandler.java
@@ -0,0 +1,37 @@
+package org.wuyang.common.Exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.wuyang.common.resp.CommonResp;
+
+/**
+ * 统一异常处理、数据预处理等
+ */
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+    /**
+     * 所有异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(Exception e) throws Exception {
+        // LOG.info("seata全局事务ID: {}", RootContext.getXID());
+        // // 如果是在一次全局事务里出异常了，就不要包装返回值，将异常抛给调用方，让调用方回滚事务
+        // if (StrUtil.isNotBlank(RootContext.getXID())) {
+        //     throw e;
+        // }
+        CommonResp commonResp = new CommonResp();
+        LOG.error("系统异常：", e);
+        commonResp.setSuccess(false);
+        commonResp.setMessage("系统出现异常，请联系管理员");
+        return commonResp;
+    }
+}
