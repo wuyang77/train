e5d9565a6d1409c4bfffd11db92800379bf3d27c
wuyang77 <2388958622@qq.com>
2026-01-15 19:24:08 +0800

    增减Login路由及页面

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link>|
+    <router-link to="/login">登录页面</router-link>
   </nav>
   <router-view/>
 </template>
