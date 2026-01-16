d1cbfcd5e1bd6da790e442cbd4bee6e72caf8456
wuyang77 <2388958622@qq.com>
2026-01-17 00:24:31 +0800

    vue组件开发增加the-header组件

---
+++ web/src/components/the-head.vue
@@ -0,0 +1,50 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo" />
+    <a-menu
+        v-model:selectedKeys="selectedKeys1"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 11</a-menu-item>
+      <a-menu-item key="2">nav 22</a-menu-item>
+      <a-menu-item key="3">nav 33</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script>
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+  name: "the-header-view",
+  setup() {
+
+
+
+    return {
+      selectedKeys1: ref(['2'])
+    };
+  },
+});
+</script>
+
+<style scoped>
+#components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+</style>
