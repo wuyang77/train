6cfaa28fea959599410300d560fbed7fd5173244
wuyang77 <2388958622@qq.com>
2026-01-25 06:16:11 +0800

    使用IDEA自带的HttpClient记住登录的信息

---
+++ member/src/main/java/org/wuyang/member/service/PassengerService.java
@@ -0,0 +1,32 @@
+package org.wuyang.member.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+import org.wuyang.common.util.SnowUtil;
+import org.wuyang.member.domain.Passenger;
+import org.wuyang.member.mapper.PassengerMapper;
+import org.wuyang.member.resq.PassengerSaveReq;
+
+@Service
+public class PassengerService {
+
+    @Resource
+    private PassengerMapper passengerMapper;
+
+    public void savePassenger(PassengerSaveReq req) {
+        DateTime now = DateTime.now();
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        if (ObjectUtil.isNull(passenger.getId())) {
+            passenger.setId(SnowUtil.getSnowflakeNextId());
+            passenger.setCreateTime(now);
+            passenger.setUpdateTime(now);
+            passengerMapper.insert(passenger);
+        } else {
+            passenger.setUpdateTime(now);
+            passengerMapper.updateByPrimaryKey(passenger);
+        }
+    }
+}
