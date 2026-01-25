fc059859fa320c789081cc3ee09be24643e15a36
wuyang77 <2388958622@qq.com>
2026-01-26 01:11:13 +0800

    使用Threadlocal线程本地存储变量会员信息，新增LoginMemberContext。添加了日志拦截器和会员登录拦截器

--- member/src/main/java/org/wuyang/member/service/PassengerService.java
+++ member/src/main/java/org/wuyang/member/service/PassengerService.java
@@ -5,6 +5,7 @@ import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import org.wuyang.common.context.LoginMemberContext;
 import org.wuyang.common.util.SnowUtil;
 import org.wuyang.member.domain.Passenger;
 import org.wuyang.member.mapper.PassengerMapper;
@@ -20,6 +21,7 @@ public class PassengerService {
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
         if (ObjectUtil.isNull(passenger.getId())) {
+            passenger.setMemberId(LoginMemberContext.getId());
             passenger.setId(SnowUtil.getSnowflakeNextId());
             passenger.setCreateTime(now);
             passenger.setUpdateTime(now);
