052faf3fafd4d4ac40d8523ac530ee637792bb74
wuyang77 <2388958622@qq.com>
2026-01-29 09:00:49 +0800

    将旅客类型下拉框的提取为常量数组

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -37,9 +37,7 @@
         </a-form-item>
         <a-form-item label="旅客类型">
           <a-select v-model:value="passenger.type">
-            <a-select-option value="1">成人</a-select-option>
-            <a-select-option value="2">儿童</a-select-option>
-            <a-select-option value="3">学生</a-select-option>
+            <a-select-option v-for="item in PASSENGER_TYPE_ARRAY" :key="item.code" :value="item.code">{{item.value}}</a-select-option>
           </a-select>
         </a-form-item>
       </a-form>
@@ -56,6 +54,7 @@ import {notification} from "ant-design-vue";
 export default defineComponent({
   name: "passenger-view",
   setup() {
+    const PASSENGER_TYPE_ARRAY = [{code: "1", value: "成人1"}, {code: "2", value: "学生1"}, {code: "3", value: "儿童1"}];
     const visible = ref(false);
     const passenger = ref({
       id: undefined,
@@ -189,7 +188,8 @@ export default defineComponent({
       onEdit,
       onDelete,
       handleOk,
-      loading
+      loading,
+      PASSENGER_TYPE_ARRAY
     };
   }
 })
