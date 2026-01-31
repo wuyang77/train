deb16d2ad626a69c1c71ac0475187f9efdaa6875
wuyang77 <2388958622@qq.com>
2026-02-01 03:22:28 +0800

    集成dom4j,读取根目录下的pom.xml文件，读取plugin中confiurationFile的路径

--- generator/src/main/java/org/wuyang/generator/server/ServerGenerator.java
+++ generator/src/main/java/org/wuyang/generator/server/ServerGenerator.java
@@ -1,20 +1,31 @@
 package org.wuyang.generator.server;
 
-import org.wuyang.generator.util.FreemarkerUtil;
+import org.dom4j.Document;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 import java.io.File;
 import java.util.HashMap;
 
 public class ServerGenerator {
     static String toPath = "generator\\src\\main\\java\\org\\wuyang\\generator\\test\\";
+    static String pomPath = "generator\\pom.xml";
+
     static {
         new File(toPath).mkdirs();
     }
 
     public static void main(String[] args) throws Exception {
-        FreemarkerUtil.initConfig("test.ftl");
-        HashMap<String, Object> param = new HashMap<>();
-        param.put("domain", "Test");
-        FreemarkerUtil.generator(toPath + "Test.java", param);
+//        FreemarkerUtil.initConfig("test.ftl");
+//        HashMap<String, Object> param = new HashMap<>();
+//        param.put("domain", "Test");
+//        FreemarkerUtil.generator(toPath + "Test.java", param);
+        SAXReader saxReader = new SAXReader();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("pom", "http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
     }
 }
