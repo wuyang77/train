c5304155f47885c4cc7a04ac462e3db272ad26f5
wuyang77 <2388958622@qq.com>
2026-02-01 03:35:13 +0800

    集成dom4j,读取generator-config-member.xml文件所在地址，读取table以及其包含表名tableName和实体名domainObjectName的节点

--- generator/src/main/java/org/wuyang/generator/server/ServerGenerator.java
+++ generator/src/main/java/org/wuyang/generator/server/ServerGenerator.java
@@ -1,6 +1,7 @@
 package org.wuyang.generator.server;
 
 import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -20,12 +21,22 @@ public class ServerGenerator {
 //        HashMap<String, Object> param = new HashMap<>();
 //        param.put("domain", "Test");
 //        FreemarkerUtil.generator(toPath + "Test.java", param);
+        String generatorPath = getGeneratorPath();
+        Document document = new SAXReader().read("generator/" + generatorPath);
+        Node table = document.selectSingleNode("//table");
+        System.out.println(table);
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText()+ "/" + domainObjectName.getText());
+    }
+
+    private static String getGeneratorPath() throws DocumentException {
         SAXReader saxReader = new SAXReader();
         HashMap<String, String> map = new HashMap<>();
         map.put("pom", "http://maven.apache.org/POM/4.0.0");
         saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
         Document document = saxReader.read(pomPath);
         Node node = document.selectSingleNode("//pom:configurationFile");
-        System.out.println(node.getText());
+        return node.getText();
     }
 }
