10aa63e03ef90f726e68b2023ffd05c195b5349a
wuyang77 <2388958622@qq.com>
2026-02-18 14:12:28 +0800

    添加注解@DisallowConcurrentExecution，禁止任务并发执行

--- batch/src/main/java/org/wuyang/train/batch/job/TestJob.java
+++ batch/src/main/java/org/wuyang/train/batch/job/TestJob.java
@@ -1,15 +1,17 @@
 package org.wuyang.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         System.out.println("TestJob TEST开始");
          try {
-             Thread.sleep(3000);
+             Thread.sleep(5000);
          } catch (InterruptedException e) {
              e.printStackTrace();
          }
