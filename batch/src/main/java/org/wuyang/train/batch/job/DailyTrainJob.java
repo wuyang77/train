72b2c7b5c9be51b915dcb520c7da2c66ca55702f
wuyang77 <2388958622@qq.com>
2026-02-27 00:23:28 +0800

    添加定时任务：生成每日车次数据

---
+++ batch/src/main/java/org/wuyang/train/batch/job/DailyTrainJob.java
@@ -0,0 +1,19 @@
+package org.wuyang.train.batch.job;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@DisallowConcurrentExecution
+public class DailyTrainJob implements Job {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainJob.class);
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        LOG.info("生成每日车次数据开始");
+        LOG.info("生成每日车次数据结束");
+    }
+}
